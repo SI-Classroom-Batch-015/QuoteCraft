f6d6077cbb8ffc88682cb5abd763cce5abf37480
shanks <hknrn40@gmail.com>
2024-08-06 11:42:22 +0200

    Icon+Farben

---
+++ QuoteCraft/QuoteCraft/QuoteCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteCraftApp.swift
+//  QuoteCraft
+//
+//  Created by Hakan Eren on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct QuoteCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
