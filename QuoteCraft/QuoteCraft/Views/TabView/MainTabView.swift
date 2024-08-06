d6e7901c36f36aba74cb02c18db3a1391743147f
shanks <hknrn40@gmail.com>
2024-08-06 13:22:47 +0200

    MVVM Struktur

---
+++ QuoteCraft/QuoteCraft/Views/TabView/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  QuoteCraft
+//
+//  Created by Hakan Eren on 06.08.24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
